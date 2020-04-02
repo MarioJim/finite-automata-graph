c6784e3ea1021bc7b5abca911a53a0af75c661ef
MarioJim <mario.emilio.j@gmail.com>
2020-04-02 17:20:12 -0600

    File upload now works

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
 document.addEventListener('readystatechange', () => {
   if (document.readyState === 'interactive') {
     console.log('Page has finished loading');
+    addDropListener();
   }
 });
+
+const ignoreEvent = (event: DragEvent) => {
+  event.stopPropagation();
+  event.preventDefault();
+};
+
+const addDropListener = () => {
+  const page = document.getElementById('page');
+  page.ondragenter = ignoreEvent;
+  page.ondragover = ignoreEvent;
+  page.ondrop = event => {
+    ignoreEvent(event);
+
+    console.log(event);
+
+    Array.from(event.dataTransfer.files).forEach(file => {
+      if (file.type !== 'text/plain') return;
+      const reader = new FileReader();
+      reader.onload = event => console.log(event.target.result);
+      reader.readAsText(file);
+    });
+  };
+};
